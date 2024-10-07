4532d6b33b4aaf96a966f639898688bd895d8ee2
Jia Xin <kangjiaxin2004@gmail.com>
2024-10-07 11:57:32 +0800

    data cleaning to get brands and count

--- data/1.py
+++ data/1.py
@@ -1,8 +1,15 @@
 import pandas as pd
 
 
-df = pd.read_csv('model_state.csv')
-df['Brand'] = df['Model'].apply(lambda x: x.split()[0])
-df['Location'] = df['Location_State'].str.replace('Penang', 'Pulau Pinang')
-df.to_csv('brand_data.csv', index=False)
+# df = pd.read_csv('model_state.csv')
+# df['Brand'] = df['Model'].apply(lambda x: x.split()[0])
+# df['Location'] = df['Location_State'].str.replace('Penang', 'Pulau Pinang')
+# df.to_csv('brand_data.csv', index=False)
 
+df1 = pd.read_csv('brand_data.csv')
+brand = df1['Brand'].value_counts().reset_index()
+#rename
+brand.columns = ['Brand', 'Count']
+print(brand)
+#save to csv 
+brand.to_csv('brand_count.csv', index=False)
