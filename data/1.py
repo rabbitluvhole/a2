f79a529befdadc576ea9dac291a0a467d13db4ab
Jia Xin <kangjiaxin2004@gmail.com>
2024-10-09 00:59:23 +0800

    ranged dot plot prep

--- data/1.py
+++ data/1.py
@@ -1,10 +1,32 @@
 import pandas as pd
 
 
-# df = pd.read_csv('model_state.csv')
+
+# # Load data from CSV
+# df = pd.read_csv('resale_car_cleaned.csv')
+
+# # Extract Brand from Model
 # df['Brand'] = df['Model'].apply(lambda x: x.split()[0])
-# df['Location'] = df['Location_State'].str.replace('Penang', 'Pulau Pinang')
-# df.to_csv('brand_data.csv', index=False)
+
+# # Create a new DataFrame with Brand and List_Price columns
+# brand_price_df = df[['Brand', 'List_Price']]
+
+# # Save the new DataFrame to a CSV file
+# brand_price_df.to_csv('brand_price.csv', index=False)
+
+
+
+
+# Load data from CSV
+df = pd.read_csv('brand_price.csv')
+
+# Convert List_Price from string to integer
+df['List_Price'] = df['List_Price'].replace({'RM ': '', ',': ''}, regex=True).astype(int)
+
+# Save the updated DataFrame to a new CSV file
+df.to_csv('brand_price_integer.csv', index=False)
+
+
 
 # df1 = pd.read_csv('brand_data.csv')
 # brand = df1['Brand'].value_counts().reset_index()
@@ -14,11 +36,13 @@ import pandas as pd
 # #save to csv 
 # brand.to_csv('brand_count.csv', index=False)
 
-df2 = pd.read_csv('brand_data.csv')
-# extract brand and location
-df2 = df2[['Brand', 'Location']]
-# group by brand and location
-df2 = df2.groupby(['Brand', 'Location']).size().reset_index(name='Count')
-print(df2)
-#save to csv
-df2.to_csv('brand_location.csv', index=False)
+# df2 = pd.read_csv('brand_data.csv')
+# # extract brand and location
+# df2 = df2[['Brand', 'Location']]
+# # group by brand and location
+# df2 = df2.groupby(['Brand', 'Location']).size().reset_index(name='Count')
+# print(df2)
+# #save to csv
+# df2.to_csv('brand_location.csv', index=False)
+
+
