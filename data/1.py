4f93f3bcdf76ad7349e7af25128cc707ff588d54
Jia Xin <kangjiaxin2004@gmail.com>
2024-10-08 21:45:17 +0800

    sankey prep

--- data/1.py
+++ data/1.py
@@ -6,10 +6,19 @@ import pandas as pd
 # df['Location'] = df['Location_State'].str.replace('Penang', 'Pulau Pinang')
 # df.to_csv('brand_data.csv', index=False)
 
-df1 = pd.read_csv('brand_data.csv')
-brand = df1['Brand'].value_counts().reset_index()
-#rename
-brand.columns = ['Brand', 'Count']
-print(brand)
-#save to csv 
-brand.to_csv('brand_count.csv', index=False)
+# df1 = pd.read_csv('brand_data.csv')
+# brand = df1['Brand'].value_counts().reset_index()
+# #rename
+# brand.columns = ['Brand', 'Count']
+# print(brand)
+# #save to csv 
+# brand.to_csv('brand_count.csv', index=False)
+
+df2 = pd.read_csv('brand_data.csv')
+# extract brand and location
+df2 = df2[['Brand', 'Location']]
+# group by brand and location
+df2 = df2.groupby(['Brand', 'Location']).size().reset_index(name='Count')
+print(df2)
+#save to csv
+df2.to_csv('brand_location.csv', index=False)
